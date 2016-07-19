ca291dbeecc46380105aff26883bd5a47460876d
Garret Meier <garret@uber.com>
2016-07-19 01:05:08 -0700

    updated tests

--- test.js
+++ test.js
@@ -4,8 +4,26 @@ var tape = require('tape');
 var a = require('./index.js');
 
 tape('testing exists', function t(assert) {
-    var msg = a.existsMsg('test');
-    a.exists(1, msg('num'));
+    var message = a.msg('test');
+    a.exists(1, message('num'));
+    assert.pass();
+    assert.end();
+});
+
+tape('testing type', function t(assert) {
+    var msg = a.msg('test');
+
+    a.exists('value', msg('string'), 'string');
+    assert.pass();
+    assert.end();
+
+});
+
+tape('testing type no value', function t(assert) {
+    var msg = a.msg('test');
+    assert.throws(function tryThrows() {
+        a.exists(null, msg('none'), 'string');
+    }, 'Assertion Error');
 
     assert.end();
 });
